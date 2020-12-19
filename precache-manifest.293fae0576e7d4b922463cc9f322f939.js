3da06ce7e4d4f9a024f45eb06d3014e211c661d6
Ankush Chourasia <ankushchourasia@hotmail.com>
2020-12-19 13:53:57 +0530

    Updates

---
+++ precache-manifest.293fae0576e7d4b922463cc9f322f939.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dd53d661fdf70bc90aae2a9a1da2f5",
+    "url": "/Credit-Card/index.html"
+  },
+  {
+    "revision": "5a8e09f17c2bfa83a8ee",
+    "url": "/Credit-Card/static/css/main.de1d1ccb.chunk.css"
+  },
+  {
+    "revision": "ac419a2a52af4172c86b",
+    "url": "/Credit-Card/static/js/2.ccac1f24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Credit-Card/static/js/2.ccac1f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8e09f17c2bfa83a8ee",
+    "url": "/Credit-Card/static/js/main.14f5a4a4.chunk.js"
+  },
+  {
+    "revision": "b57599ef07f548c78308",
+    "url": "/Credit-Card/static/js/runtime-main.bba493cb.js"
+  },
+  {
+    "revision": "310ee88474631cf53c172911adcdd136",
+    "url": "/Credit-Card/static/media/background.310ee884.png"
+  },
+  {
+    "revision": "791aeaf7dd6fa2dd7ca9b13d79943cd2",
+    "url": "/Credit-Card/static/media/chip.791aeaf7.png"
+  }
+]);
